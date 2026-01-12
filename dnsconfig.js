a8cb3bf12f4beb64a62e48f5d6dc7e3ccda5e2d0
Jonathan Beliën <dev@jbelien.be>
2026-01-12 19:45:11 +0100

    Update dnsconfig.js

--- dnsconfig.js
+++ dnsconfig.js
@@ -73,6 +73,7 @@ D(
   MX("@", 10, "in1-smtp.messagingengine.com."),
   MX("@", 20, "in2-smtp.messagingengine.com."),
   TXT("@", "v=spf1 include:spf.messagingengine.com include:_spf.google.com ?all"),
+  TXT("_dmarc", "v=DMARC1; p=none;"),
   CNAME("fm1._domainkey", "fm1.osm.be.dkim.fmhosted.com."),
   CNAME("fm2._domainkey", "fm2.osm.be.dkim.fmhosted.com."),
   CNAME("fm3._domainkey", "fm3.osm.be.dkim.fmhosted.com."),
